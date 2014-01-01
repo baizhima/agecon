88ca1e1a43f9f6b41d7c54239133b0e9fe9c01f2
baizhima <oh_baizhima@163.com>
2014-01-01 12:56:44 +0800

    Statistical Software Application course material updated

--- final.R
+++ final.R
@@ -1,5 +1,7 @@
 # Statistical Software Application, SARD Fall 2013, RUC
 # Final exam code, Shan Lu
+
+################################################################
 # Chapter 3 
 # Question 2 AER pp.100
 rm(list=ls())
@@ -65,18 +67,101 @@ anova(m1,aug_m)
 
 # Problem 9 pp.101
 rm(list=ls()) # clean workspace
-data("USConsump1993") # include data
+data("USConsump1993") # include data, mts format
 # (a) by OLS
 consump_lm <- lm(expenditure ~ income, data = USConsump1993)
 
 
 # (b) by IV. The only available instrument is investment, given by the
 #     identity expenditure + investment = income.
-consump_iv <- ivreg(expenditure ~ income | income - expenditure,data = USConsump1993)
+dfUSConsump <- data.frame(USConsump1993)
+dfUSConsump$investment <- dfUSConsump$income - dfUSConsump$expenditure
+consump_iv2 <- ivreg(expenditure ~ income | investment, data = dfUSConsump)
 
 # (c) Compare both sets of estimates using a Hausman test, thus replicating
 #     Baltagi (2002, Section 11.7). What do you conclude?
-install.packages(plm) # linear regression w/ panel data package
-require(plm)
-dfcomp <- data.frame(USConsump1993)
+summary(consump_lm)
+summary(consump_iv2)
 
+#####################################################
+# Chapter 4
+# Question 3, pp.128 
+# Explore further growth regressions for the OECDGrowth data using the augmented
+# and extended Solow models of Nonneman and Vanhoudt (1996),
+# which consider the additional regressors log(school) (human capital)
+# and log(randd) (technological know-how), respectively. First, replicate
+# the OLS results from Nonneman and Vanhoudt (1996, Table IV), and
+# subsequently compare them with the resistant LTS results by adopting
+# the strategy of Zaman et al. (2001).
+
+rm(list=ls()) # clean workspace
+data("OECDGrowth")
+
+# source: ?OECDGrowth OECD Macroeconomic Data Documentation
+## augmented and extended Solow growth model
+## Nonneman and Vanhoudt (1996), Table IV
+aso_ols <- lm(log(gdp85/gdp60) ~ log(gdp60) + log(invest) +
+                log(school) + log(popgrowth+.05), data = OECDGrowth)
+eso_ols <- lm(log(gdp85/gdp60) ~ log(gdp60) + log(invest) +
+                log(school) + log(randd) + log(popgrowth+.05), data = OECDGrowth)
+## determine unusual observations using LTS
+library("MASS")
+so_lts <- lqs(log(gdp85/gdp60) ~ log(gdp60) +  log(invest) + log(popgrowth+.05),
+              data = OECDGrowth, psamp = 13, nsamp = "exact")
+## large residuals
+nok1 <- abs(residuals(so_lts))/so_lts$scale[2] > 2.5
+residuals(so_lts)[nok1]/so_lts$scale[2]
+## high leverage
+so_ols <- lm(log(gdp85/gdp60) ~ log(gdp60) + log(invest) + log(popgrowth+.05),
+             data = OECDGrowth) ## Zaman, Rousseeuw and Orhan (2001), Table 2
+X <- model.matrix(so_ols)[,-1]
+cv <- cov.rob(X, nsamp = "exact")
+mh <- sqrt(mahalanobis(X, cv$center, cv$cov))
+nok2 <- mh > 2.5
+mh[nok2]
+
+## bad leverage
+nok <- which(nok1 & nok2)
+nok
+
+## robust results without bad leverage points
+so_rob <- update(so_ols, subset = -nok)
+summary(so_rob)
+# This is similar to Zaman, Rousseeuw and Orhan (2001), Table 2
+# but uses exact computations (and not sub-optimal results
+# for the robust functions lqs and cov.rob) 
+
+##############################################################
+# Chapter 5
+# Question 2, pp.158 Probit model
+rm(list=ls())
+
+data("PSID1976")
+
+## Example 21.4, p. 681, and Tab. 21.3, p. 682
+# (a) Fit a probit model for labor force participation using the regressors
+#     age, age squared, family income, education, and a factor indicating
+#     the presence of children. (The factor needs to be constructed from the available information.)
+PSID1976$kids <- with(PSID1976, factor((youngkids + oldkids) > 0,
+                                       levels = c(FALSE, TRUE), labels = c("no", "yes")))
+gr_probit1 <- glm(participation ~ age + I(age^2) + I(fincome/10000) + education + kids,
+                  data = PSID1976, family = binomial(link = "probit") )  
+# (b) Reestimate the model assuming that different equations apply to
+#     women with and without children.
+gr_probit2 <- glm(participation ~ age + I(age^2) + I(fincome/10000) + education,
+                  data = PSID1976, family = binomial(link = "probit"))
+gr_probit3 <- glm(participation ~ kids/(age + I(age^2) + I(fincome/10000) + education),
+                  data = PSID1976, family = binomial(link = "probit"))
+# (c) Perform a likelihood ratio test to check whether the more general
+#     model is really needed.
+lrtest(gr_probit1)
+
+# 5.Using the PSID1976 data, run a tobit regression of hours worked on nonwife
+#   income (to be constructed from the available information), age, experience,
+#   experience squared, education, and the numbers of younger and
+#   older children.
+
+
+mc_tobit <- tobit(hours ~ nwincome + education + experience + I(experience^2) + age
+                  +youngkids + oldkids, data = PSID1976)
+coeftest(mc_tobit)
